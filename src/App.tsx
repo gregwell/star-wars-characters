12bb4a2d30c80a8fff1cf443e436057c9d3e5b6c
DESKTOP-27CQTIN\grzegurz <grzegorzstudzinski@hotmail.com>
2021-04-03 18:00:06 +0200

    infinite scroll - initial implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useCallback, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import useAsync from "./hooks/useAsync";
@@ -7,28 +7,71 @@ import { Character } from "./types/types";
 
 function App() {
   const [pageNumber, setPageNumber] = useState(1);
-
   const { status, characters, error } = useAsync(fetchCharacters, pageNumber);
 
   const loadMoreResults = () => {
     setPageNumber((prevPageNumber) => prevPageNumber + 1);
   };
 
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  const loadMore = useCallback(
+    (entries) => {
+      const target = entries[0];
+      if (target.isIntersecting) {
+        status === "success" && loadMoreResults();
+      }
+    },
+    [status]
+  );
+
+  useEffect(() => {
+    const options = {
+      threshold: 1.0
+  };
+    const observer = new IntersectionObserver(loadMore, options);
+    const currentRef = ref.current;
+    if (ref && currentRef) {
+      observer.observe(currentRef);
+    }
+  },[ref, loadMore]);
+
+  /*
+  const lastCharacterRef = useCallback(
+    (node) => {
+      if (status==='pending') return;
+        ref.current = new IntersectionObserver(entries => {
+        if(entries[0].isIntersecting) {
+          setPageNumber(prevPageNumber => prevPageNumber +1)
+        }
+      })
+    },
+    [status]
+  )
+  */
+
   return (
     <div className="app-container">
       <Header />
       <div>
         <p>
-          {status === "success" && (
+
             <>
               success!
-              {characters.map((objectMapped: Character, index: number) => {
-                return <p key={index}>{objectMapped.name}</p>;
+              {characters.map((character: Character, index: number) => {
+                
+                  return (
+                    <p key={index}>
+                      {character.name}
+                    </p>
+                  );
+                
               })}
+              <div ref={ref}></div>
             </>
-          )}
+          
           {status === "error" && { error }}
-          {status === "pending" && "pending..."}
+          {status === "pending" && "loading..."}
           {status === "idle" && "idle..."}
         </p>
       </div>
